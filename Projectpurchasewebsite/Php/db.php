cc11077c93fae4ca7c74f8ab53d18d28622d72e8
Hob-Rob <robert.cresswell23@gmail.com>
2025-03-27 12:26:28 +0000

    Update db.php
    - redid pdo command

--- Projectpurchasewebsite/Php/db.php
+++ Projectpurchasewebsite/Php/db.php
@@ -6,7 +6,7 @@ $password = "root"; // Default for localhost (use your own password if set)
 $database = "SALES"; // Your database name
  
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo = new PDO("mysql:host=localhost;dbname=USERS", "stockpro", "stockpro");
     // Set error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
